d4574bbcc23c462e99fc0916cc18c5281a239e53
ayumitakayuki <151020758+ayumitakayuki@users.noreply.github.com>
2023-11-16 17:18:27 +0700

    Create Jawaban2.sql

---
+++ UTS/Jawaban2.sql
@@ -0,0 +1,11 @@
+INSERT INTO matakuliah
+VALUES (1, "Kalkulus 1", "4321", "Nagita Slavina", "4 SKS"),
+			 (2, "Statistik", "9876", "Tasya Farasya", "S3 SKS");
+			 
+INSERT INTO mahasiswa
+VALUES (1, 'Masami', 'Fujisawa', 'Kalkulus 1', '20220801563', 1),
+			 (2, 'Takayuki', 'Fujisawa', 'Statistik', '20220801717', 2);
+			
+INSERT INTO dosen
+VALUES (1, "1717", "Nagita", "Slavina", "Kalkulus 1", 1),
+			 (2, "7171", "Tasya", "Farasya", "Statistik", 2);
